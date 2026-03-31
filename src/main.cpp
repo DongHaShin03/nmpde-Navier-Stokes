66dc760bd600af06b8f72fe5a127a79a71224f59
GiovanniVaccaro <giovannivaccaro2@gmail.com>
2026-03-31 20:53:44 +0200

    setup main and prm file

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-#include "NavierStokes2D.hpp"
+#include "FlowPastCylinder2D.hpp"
 
-// Main function.
-int
-main(int argc, char *argv[])
+static constexpr unsigned int dim = NavierStokes2D::dim;
+
+int main(int argc, char *argv[])
 {
-  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+    Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
-  const std::string  mesh_file_name  = "../mesh/navierstokesFINAL.msh";
-  const unsigned int degree_velocity = 2;
-  const unsigned int degree_pressure = 1;
+    const std::string parameter_file =
+      (argc > 1 ? std::string(argv[1]) : "../flow_past_cylinder_2d.prm");
 
-  NavierStokes2D problem(mesh_file_name, degree_velocity, degree_pressure);
+    const FlowPastCylinder2DParameters parameters =
+      FlowPastCylinder2DParameters::read(parameter_file);
 
-  problem.run();
+    const auto f = [](const Point<dim> &, const double &)
+    {
+        Tensor<1, dim> result;
+        result[0] = 0.0;
+        result[1] = 0.0;
+        return result;
+    };
 
-  return 0;
-} 
+    NavierStokes2D problem(parameters.mesh_file_name,
+                           parameters.degree_velocity,
+                           parameters.degree_pressure,
+                           parameters.nu,
+                           f,
+                           parameters.T,
+                           parameters.theta,
+                           parameters.delta_t);
+
+    FlowPastCylinder2DCase benchmark_case(parameters);
+    benchmark_case.apply_to(problem);
+
+    problem.run();
+
+    return 0;
+}
